2bdc6f6a054b0b4e4805e847fb04487f5696fbab
MATTEO BOLLO s332129 <s332129@studenti.polito.it>
2024-10-07 12:34:48 +0200

    version 0.1: set-up for develop API for service management

---
+++ code/server/src/dao/serviceDAO.ts
@@ -0,0 +1,22 @@
+import db from "../db/db";
+import {Service} from "../components/service";
+
+class ServiceDAO {
+    addService(name: string){
+        return;
+    }
+
+    getService(id: string): Service {
+        return;
+    }
+
+    deleteService(id: string){
+        return;
+    }
+    
+    editService(id: string, name: string) {
+        return;
+    }
+}
+
+export default ServiceDAO;
