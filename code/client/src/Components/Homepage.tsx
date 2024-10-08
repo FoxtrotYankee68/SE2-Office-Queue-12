708fdacbf1e5c2e8b841b6b7e0490a8c35244821
dimitrievskamilica <milica.dimitrievska@student.um.si>
2024-10-08 17:17:09 +0200

    adding GUI for Admin page and Services

--- code/client/src/Components/Homepage.tsx
+++ code/client/src/Components/Homepage.tsx
@@ -1,14 +1,28 @@
-import {Button, Card, Container, Row} from "react-bootstrap"
+import {Button, Card, Container, Row, Navbar} from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./style.css"
 import API from "../API/API";
 
 function Homepage() {
     const navigate = useNavigate()
 
+
     return (
         <>
+        
             <Container fluid>
+            <Navbar style={{ backgroundColor: '#FF7F50' }}>
+             <Container background-colour="#FF7F50">
+                <Navbar.Brand href="#home">
+                <i className="bi bi-building"></i>{' '}
+                  Office Queue
+                </Navbar.Brand>
+                <Button variant="outline-light" className="ms-auto" onClick={() => navigate("/admin")}>
+                   Switch to Admin
+                </Button>
+             </Container>
+            </Navbar>
                 <Row style={{ textAlign: "center", margin: 0, padding: 0 }}>
                     <span className="adminTitle">Homepage</span>
                 </Row>
