9fc278d169e93b445ee277c3704d65bb23b3ac66
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2024-10-09 20:32:01 +0200

    Add the API.ts in the client for the counter

--- code/client/src/API/API.ts
+++ code/client/src/API/API.ts
@@ -1,14 +1,18 @@
+import { Counter } from "../Models/Counter";
+import { Service } from "../Models/service";
+
 const baseURL = "http://localhost:3001/officequeue/"
 
-/** ------------------- Item APIs ------------------------ */
+/** ------------------- Counter APIs ------------------------ */
 
-async function addItem(id: string) {
-    let response = await fetch(baseURL + "items/" + id, {
+async function addCounter(name: string) {
+    let response = await fetch(`${baseURL}counters`, {
         method: 'POST',
-        credentials: "include",
+        /*credentials: "include",*/
         headers: {
             "Content-Type": "application/json"
-        }
+        },
+        body: JSON.stringify({name})
     })
     if (response.ok) {
         return
@@ -22,10 +26,16 @@ async function addItem(id: string) {
     }
 }
 
-async function getItem(id: string) {
-    const response = await fetch(baseURL + "items/" + id, { credentials: "include" })
+async function getCounter(id: number) {
+    const response = await fetch(`${baseURL}counters/${id}`, { 
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
     if (response.ok) {
-        return await response.json()
+        const counterJson = await response.json();
+        return new Counter(counterJson.id, counterJson.name);
     } else {
         const errDetail = await response.json();
         if (errDetail.error)
@@ -36,8 +46,137 @@ async function getItem(id: string) {
     }
 }
 
+async function getAllCounters() {
+    const response = await fetch(`${baseURL}counters`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    if (response.ok) {
+        const countersJson = await response.json();
+        const counters = countersJson.map((c: any) => new Counter(c.id, c.name))
+        return counters;
+    } else {
+        const errDetail = await response.json();
+        if (errDetail.error)
+            throw errDetail.error
+        if (errDetail.message)
+            throw errDetail.message
+        throw new Error("Error. Please reload the page")
+    }
+}
+
+async function editCounter(id: number, name: string) {
+    const response = await fetch(`${baseURL}counters/${id}`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name})
+    })
+
+    if (response.ok) {
+        return;
+    } else {
+        const errDetail = await response.json();
+        if (errDetail.error)
+            throw errDetail.error
+        if (errDetail.message)
+            throw errDetail.message
+        throw new Error("Error. Please reload the page")
+    }
+}
+
+async function deleteCounter(id: number) {
+    const response = await fetch(`${baseURL}counters/${id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+
+    if (response.ok) {
+        return;
+    } else {
+        const errDetail = await response.json();
+        if (errDetail.error)
+            throw errDetail.error
+        if (errDetail.message)
+            throw errDetail.message
+        throw new Error("Error. Please reload the page")
+    }
+}
+
+async function addCounterService(counterId: number, serviceId: number) {
+    const response = await fetch(`${baseURL}counters/${counterId}/services/${serviceId}`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    if (response.ok) {
+        return;
+    } else {
+        const errDetail = await response.json();
+        if (errDetail.error)
+            throw errDetail.error
+        if (errDetail.message)
+            throw errDetail.message
+        throw new Error("Error. Please reload the page")
+    }
+}
+
+async function deleteCounterService(counterId: number, serviceId: number) {
+    const response = await fetch(`${baseURL}counters/${counterId}/services/${serviceId}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    if (response.ok) {
+        return;
+    } else {
+        const errDetail = await response.json();
+        if (errDetail.error)
+            throw errDetail.error
+        if (errDetail.message)
+            throw errDetail.message
+        throw new Error("Error. Please reload the page")
+    }
+}
+
+async function viewAllServicesByCounterToday(counterId: number) {
+    const response = await fetch(`${baseURL}counters/${counterId}/services`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    if (response.ok) {
+        const servicesJson = await response.json();
+        const services = servicesJson.map((s: any) => new Service(s.id, s.name, s.serviceTime));
+        return services;
+    } else {
+        const errDetail = await response.json();
+        if (errDetail.error)
+            throw errDetail.error
+        if (errDetail.message)
+            throw errDetail.message
+        throw new Error("Error. Please reload the page")
+    }
+    
+}
+
 const API = {
-    addItem, getItem
+    addCounter, 
+    getCounter, 
+    getAllCounters,
+    editCounter, 
+    deleteCounter, 
+    addCounterService, 
+    deleteCounterService, 
+    viewAllServicesByCounterToday
 }
 
 export default API
---
+++ code/client/src/Models/counter.ts
@@ -0,0 +1,14 @@
+/**
+ * Represents a counter
+ */
+class Counter {
+    id: number;
+    name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+export { Counter }
